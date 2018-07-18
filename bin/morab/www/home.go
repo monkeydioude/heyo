001807d94a40284d7f6860a90b5d5b2b855273bb
Julien DURIS <julien.duris@radiofrance.com>
2018-07-18 18:38:38 +0200

    need to go deep

--- bin/morab/www/home.go
+++ bin/morab/www/home.go
@@ -11,22 +11,13 @@ type Home struct {
 
 // GetHome display home
 func GetHome() ([]byte, int, error) {
-	// t, err := template.ParseFiles("www/home.html")
-
-	// if err != nil {
-	// 	log.Printf("[ERR ] Could not parse file. Reason: %s", err)
-	// 	return err500()
-	// }
-	// b := &bytes.Buffer{}
-	// t.Execute(b, map[string]string{"test": "bonjour"})
-
-	// return b.Bytes(), 200, nil
-
 	tmp := template.NewEngine("www/layout.html")
-	tmp.WithChild("www/home.html", "Content")
-	tmp.AddValue(&Home{
-		Title: "Bonjour",
-		Body:  "c'est cool",
-	})
+	tmp.
+		WithChild("www/home.html", "Content").
+		AddValue(&Home{
+			Title: "Bonjour",
+			// Body:  "c'est cool",
+		}).
+		WithChild("www/list.html", "List")
 	return tmp.Render(), 200, nil
 }
