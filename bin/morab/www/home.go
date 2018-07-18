063f6003a806c86ac44a7dfba4523848039a9433
Julien DURIS <julien.duris@radiofrance.com>
2018-07-18 15:25:27 +0200

    try to go deeep

--- bin/morab/www/home.go
+++ bin/morab/www/home.go
@@ -1,26 +1,32 @@
 package www
 
 import (
-	"bytes"
-	"html/template"
-	"log"
+	"github.com/monkeydioude/moon/template"
 )
 
-type home struct {
-	Msg  string
-	Code int
+type Home struct {
+	Title string
+	Body  string
 }
 
 // GetHome display home
 func GetHome() ([]byte, int, error) {
-	t, err := template.ParseFiles("www/home.html")
+	// t, err := template.ParseFiles("www/home.html")
 
-	if err != nil {
-		log.Printf("[ERR ] Could not parse file. Reason: %s", err)
-		return err500()
-	}
-	b := &bytes.Buffer{}
-	t.Execute(b, home{"wesh", 200})
+	// if err != nil {
+	// 	log.Printf("[ERR ] Could not parse file. Reason: %s", err)
+	// 	return err500()
+	// }
+	// b := &bytes.Buffer{}
+	// t.Execute(b, map[string]string{"test": "bonjour"})
 
-	return b.Bytes(), 200, nil
+	// return b.Bytes(), 200, nil
+
+	tmp := template.NewEngine("www/layout.html")
+	tmp.WithChild("www/home.html", "Content")
+	tmp.AddValue(&Home{
+		Title: "Bonjour",
+		Body:  "c'est cool",
+	})
+	return tmp.Render(), 200, nil
 }
